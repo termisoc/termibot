8fad3641f56d7a9abf49aee37bcdb56eb5d14cb2
Benjamin A'Lee <bma@bma.li>
2012-08-28 14:06:39 +0100

    Support wildcard subcommands.

--- ircbot/plugin.py
+++ ircbot/plugin.py
@@ -6,8 +6,11 @@ import inspect
 class Plugin(object):
     def get_subcommand(self, subcommand, user, channel, args):
         parent = inspect.stack()[1][3]
-        if parent + '_' + subcommand not in dir(self):
-            return u'Unknown subcommand %s' % subcommand
-        else:
-            return getattr(self,
-                    parent + '_' + subcommand)(user, channel, args)
+        command = parent + '_' + subcommand
+        if command not in dir(self):
+            if parent + '_ALL' not in dir(self):
+                return u'Unknown subcommand %s' % subcommand
+            else:
+                command = parent + '_ALL'
+                args.insert(0, subcommand)
+        return getattr(self, command)(user, channel, args)

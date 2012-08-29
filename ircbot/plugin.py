a444717776e29cb6a3d5b229e676eacf5579f44f
Benjamin A'Lee <bma@bma.li>
2012-08-29 01:30:02 +0100

    Slight simplification.

--- ircbot/plugin.py
+++ ircbot/plugin.py
@@ -8,9 +8,9 @@ class Plugin(object):
         parent = inspect.stack()[1][3]
         command = parent + '_' + subcommand
         if command not in dir(self):
-            if parent + '_ALL' not in dir(self):
+            command = parent + '_ALL'
+            if command not in dir(self):
                 return u'Unknown subcommand %s' % subcommand
             else:
-                command = parent + '_ALL'
                 args.insert(0, subcommand)
         return getattr(self, command)(user, channel, args)
